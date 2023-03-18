7e24fcb79fbe470242661743895e45d0edf223f4
djibrilnaji <najidjibril@gmail.com>
2023-03-18 15:37:31 +0100

    Revert "add endpoint for view all roles and specific roles"
    This reverts commit 15096bd9fa83d07c8fb3d466e64161394c1a971b.

--- src/prepareRoutes.js
+++ src/prepareRoutes.js
@@ -1,11 +1,9 @@
-import prepareRolesRoutes from "./routes/prepareRolesRoutes.js"
 import prepareSignRoutes from "./routes/prepareSignRoutes.js"
 import prepareUsersRoutes from "./routes/prepareUsersRoutes.js"
 
 const prepareRoutes = (ctx) => {
   prepareSignRoutes(ctx)
   prepareUsersRoutes(ctx)
-  prepareRolesRoutes(ctx)
 }
 
 export default prepareRoutes
